a4f9b0fe2eabde0492610120ffa5847c79391c14
Tisha Gupta <tishaguptatop@gmail.com>
2024-02-12 18:56:25 +0530

    new-project

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import './App.css'
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  return (
+     <Router>
+      <Header/>
+      <Routes>
+        <Route/>
+      </Routes>
+      <Footer/>
+     </Router>
+     
+  )
+}
+
+export default App
